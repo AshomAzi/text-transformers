7c54c86f5ccdd651463ccd5b6f5f4cad1cc33618
AshomAzi <ashomazibulus@gmail.com>
2026-06-22 18:21:43 +0100

    Refactor and added ReadAndWrite function

--- transformers/main.go
+++ transformers/main.go
@@ -6,32 +6,34 @@ import (
 	"os"
 )
 
-func main() {
-	input := os.Args
-	if len(input) == 3 {
-		file1, err := os.Open(input[1])
-		if err == nil {
-			file2, err := os.Create(input[2])
-			if err == nil {
-				reader := bufio.NewScanner(file1)
-				writer := bufio.NewWriter(file2)
-				for reader.Scan() {
-					text := reader.Text()
-					writer.WriteString(text);writer.WriteString("\n")
-				}
-				if err := reader.Err(); err != nil {
-					fmt.Println(err)
-				}
-				writer.Flush()
-			} else {
-				fmt.Println(err)
-			}
-			defer file2.Close()
-		} else {
-			fmt.Println(err)
-		}
-		defer file1.Close()
-	} else {
-		fmt.Println("Expects two input, Run program with the command below\ngo run . <input filename> <output filename>")
+func ReadAndWrite() {
+	if len(os.Args) != 3 {
+		fmt.Println("Expects 3 arguments. Run program with the following command\ngo run . <input filename> <output filename>")
+		return
 	}
+	inputFile := os.Args[1]
+	outputFile := os.Args[2]
+
+	readFile, err := os.Open(inputFile)
+	if err == nil {
+		writeFile, err := os.Create(outputFile)
+		if err == nil {
+			reader := bufio.NewScanner(readFile)
+			if reader.Err() != nil {
+				fmt.Println("An error occurred while reading file")
+			}
+			writer := bufio.NewWriter(writeFile)
+			for reader.Scan() {
+				text := reader.Text()
+				writer.WriteString(text);writer.WriteString("\n")
+			}
+			writer.Flush()
+		}
+		defer writeFile.Close()
+	}
+	defer readFile.Close()
+}
+
+func main() {
+	ReadAndWrite()
 }
